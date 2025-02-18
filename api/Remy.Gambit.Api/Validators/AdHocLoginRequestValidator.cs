fe038735f0133e395e064474f884cdbe1eafc54f
Jason Maglaya <jason.maglaya@ap.jll.com>
2025-02-19 01:29:38 +0800

    Balance Inquiry

--- api/Remy.Gambit.Api/Validators/AdHocLoginRequestValidator.cs
+++ api/Remy.Gambit.Api/Validators/AdHocLoginRequestValidator.cs
@@ -7,8 +7,8 @@ namespace Remy.Gambit.Api.Validators
     {
         public AdHocLoginRequestValidator()
         {
-            RuleFor(request => request.ClientId).NotNull().NotEmpty().WithMessage("ClientId is required");
-            RuleFor(request => request.ClientSecret).NotNull().NotEmpty().WithMessage("ClientSecret is required");
+            //RuleFor(request => request.ClientId).NotNull().NotEmpty().WithMessage("ClientId is required");
+            //RuleFor(request => request.ClientSecret).NotNull().NotEmpty().WithMessage("ClientSecret is required");
             RuleFor(request => request.Type).NotNull().NotEmpty().WithMessage("Type is required");
             RuleFor(request => request.UserName).NotNull().NotEmpty().WithMessage("UserName is required");
         }
